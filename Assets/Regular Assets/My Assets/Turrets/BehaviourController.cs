7c57aac4e3052aede99a2afd488624c0edb69970
forresttheman <96036883+forresttheman@users.noreply.github.com>
2022-04-04 20:55:58 -0700

    (stable) lots of improvements !!

---
+++ Assets/Regular Assets/My Assets/Turrets/BehaviourController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class BehaviourController : MonoBehaviour
+{
+    public bool isIn3rd = true;
+    public GameObject camThird;
+    public GameObject camFirst;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("q")) //add a collision check to the turret so they can't just become a turret from anywhere
+        {
+            isIn3rd = !isIn3rd;
+        }
+
+        if (isIn3rd)
+        {
+            camThird.SetActive(true);
+            camFirst.SetActive(false);
+        }
+
+        else
+        {
+            camThird.SetActive(false);
+            camFirst.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Regular Assets/My Assets/Turrets/TurretTypeSelect.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class TurretTypeSelect : MonoBehaviour
+{
+    public GameObject[] Turrets;
+    private int turretIndex = 0;
+    public int desiredTurret = 0;
+    private GameObject tower;
+
+    private GameObject scriptHolder;
+    public TurretControllerWASD turretController;
+
+
+    void SelectTurret()
+    {
+        if (desiredTurret != turretIndex)
+        {
+            foreach (GameObject turret in Turrets)
+            {
+                turretIndex += 1;
+
+                if (turretIndex == desiredTurret)
+                {
+                    turret.SetActive(true);
+                    
+                    //find the tower in hierarchy
+                    tower = GameObject.FindGameObjectWithTag("Tower");
+
+                    //find the transform component and set the turret controller transform to it
+                    turretController.Tower = tower.GetComponent<Transform>();
+                }
+
+                else
+                {
+                    turret.SetActive(false);
+                }
+                
+                if (turretIndex > Turrets.Length)
+                {
+                    break;
+                }
+            }
+        }
+
+        else {
+            foreach (GameObject turret in Turrets)
+            {
+                turret.SetActive(true);
+                break;
+            }
+        }
+    }
+
+    void Start()
+    {
+        scriptHolder = GameObject.Find("Base");
+        turretController = scriptHolder.GetComponent<TurretControllerWASD>();
+        SelectTurret();
+    }
+}
