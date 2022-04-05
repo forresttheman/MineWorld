6f51afd94559e3272f310fd099c59704861829fb
forresttheman <96036883+forresttheman@users.noreply.github.com>
2022-04-04 17:43:40 -0700

    removed redundant things

--- Assets/TurretTypeSelect.cs
+++ Assets/TurretTypeSelect.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TurretTypeSelect : MonoBehaviour
@@ -24,39 +22,33 @@ public class TurretTypeSelect : MonoBehaviour
                 if (turretIndex == desiredTurret)
                 {
                     turret.SetActive(true);
-                    Debug.Log("Desired Turret Selected");
-
+                    
                     //find the tower in hierarchy
                     tower = GameObject.FindGameObjectWithTag("Tower");
 
                     //find the transform component and set the turret controller transform to it
                     turretController.Tower = tower.GetComponent<Transform>();
-
                 }
 
                 else
                 {
                     turret.SetActive(false);
-                    Debug.Log("Skipped Turret");
                 }
                 
                 if (turretIndex > Turrets.Length)
                 {
-                    Debug.Log("Done.");
                     break;
                 }
             }
         }
 
         else {
-            Debug.Log("turrrrr");
             foreach (GameObject turret in Turrets)
             {
                 turret.SetActive(true);
                 break;
             }
         }
-
     }
 
     void Start()
@@ -64,20 +56,5 @@ public class TurretTypeSelect : MonoBehaviour
         scriptHolder = GameObject.Find("Base");
         turretController = scriptHolder.GetComponent<TurretControllerWASD>();
         SelectTurret();
-        Debug.Log("Success");
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown("p"))
-        {
-            desiredTurret += 1;
-            SelectTurret();
-        }
-
-        if (desiredTurret >= Turrets.Length)
-        {
-            desiredTurret = Turrets.Length;
-        }
     }
 }
